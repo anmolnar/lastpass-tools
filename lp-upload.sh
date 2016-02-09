2738d694edfafc28e09494dbfb3b49c804410fc1
Andor Molnar <andor.molnar@logmein.com>
2016-02-09 17:57:43 +0100

    Added cli parser and text mode

--- lp-upload.sh
+++ lp-upload.sh
@@ -1,30 +1,63 @@
 #!/bin/bash
 
 lp="/home/anmolnar/lastpass/lastpass-cli/lpass"
-folder="SSH"
-template="xively-dev.pem"
-upload_file="$1"
+FOLDER="SSH"
+TEMPLATE="xively-dev.pem"
+TEXT_MODE=NO
+
+while [[ $# > 0 ]]
+do
+    key="$1"
+
+    case $key in
+        -f|--folder)
+        FOLDER="$2"
+        shift # past argument
+        ;;
+        -t|--template)
+        TEMPLATE="$2"
+        shift # past argument
+        ;;
+        -u|--upload-file)
+        UPLOAD_FILE="$2"
+        shift # past argument
+        ;;
+        --text)
+        TEXT_MODE=YES
+        ;;
+        *)
+                # unknown option
+        ;;
+    esac
+    shift # past argument or value
+done
 
 function search_by_name {
-    search=`$lp ls $folder | grep $1`
+    search=`$lp ls $FOLDER | grep "$1 \["`
     id=`expr "$search" : 'SSH/.*id: \([0-9]*\)'`
     echo $id
 }
 
 function search_by_name_except {
-    search=`$lp ls $folder | grep $1 | grep -v $2`
+    search=`$lp ls $FOLDER | grep "$1 \[" | grep -v $2`
     id=`expr "$search" : 'SSH/.*id: \([0-9]*\)'`
     echo $id
 }
 
-if [ ! -r $upload_file ];
+if [ "$UPLOAD_FILE" == "" ];
 then
-    echo "Unable to read file to upload: $upload_file"
+    echo "You must specify the file to upload with -u or --upload-file."
+    exit 0
+fi
+
+if [ ! -r "$UPLOAD_FILE" ];
+then
+    echo "Unable to read file to upload: $UPLOAD_FILE"
     exit 1
 fi
 
-echo "Upload file: $upload_file"
-up_basename=`basename $upload_file`
+echo "Upload file: $UPLOAD_FILE"
+up_basename=`basename $UPLOAD_FILE`
 f_id=$(search_by_name $up_basename)
 if [ "$f_id" != "" ];
 then
@@ -32,17 +65,18 @@ then
     exit 1
 fi
 
-echo "Template file: $template"
-template_id=$(search_by_name $template)
+echo "Template file: $TEMPLATE"
+template_id=$(search_by_name $TEMPLATE)
 echo "Template file ID: $template_id"
 
-$lp duplicate $template_id
+#$lp duplicate $template_id
 echo "Template duplicated"
 
 while true;
 do
-    new_id=$(search_by_name_except $template $template_id)
-    if [[ "$new_id" =~ ^[0-9]{19} ]];
+    new_id=$(search_by_name_except $TEMPLATE $template_id)
+    echo $new_id
+    if [[ "$new_id" =~ ^[0-9]{18} ]];
     then
         break;
     fi
@@ -52,11 +86,16 @@ done
 
 echo "New file ID: $new_id"
 
-fullname=$folder/$up_basename
+fullname=$FOLDER/$up_basename
 echo $fullname | $lp edit --non-interactive --name $new_id
 echo "File renamed to $fullname" 
 
-base64 $upload_file | $lp edit --non-interactive --notes $new_id
+if [ "$TEXT_MODE" == "YES" ];
+then
+    cat $UPLOAD_FILE | $lp edit --non-interactive --notes $new_id
+else
+    base64 $UPLOAD_FILE | $lp edit --non-interactive --notes $new_id
+fi
 echo "File content uploaded"
 
 
